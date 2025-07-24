7fe3d791bc710d66aeedd12f1f6ee38b1ce0ccab
faisal-gif <fagdera@gmail.com>
2025-07-24 12:57:33 +0700

    refactor: Remove GTranslateWidget from GuestLayout

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -2,14 +2,12 @@ import { useEffect } from 'react';
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import Footer from '@/Components/Footer';
 import Navigation from '@/Components/Navigation';
-import GTranslateWidget from '@/Components/GTranslateWidget';
 
 export default function GuestLayout({ children }) {
 
     return (
         <div className="min-h-screen bg-white">
-            
-          <GTranslateWidget />
+
             <Navigation />
             {children}
             <Footer />
